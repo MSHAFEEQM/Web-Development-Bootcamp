754df744a1c579a73b68be9559de69d73b7ac31a
M_SHAFEEQ_M <m_shafeeq_m@localhost.localdomain>
2024-02-15 10:35:45 -0100

    REST ful APIs Created

--- Back End/5.5 REST APIs/solution.js
+++ Back End/5.5 REST APIs/solution.js
@@ -6,8 +6,8 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
-//Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+//Add your own bearer token from the previous lesson.  
+const yourBearerToken = "bd0cce79-44dd-460b-9d6e-7812b73a6306";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
