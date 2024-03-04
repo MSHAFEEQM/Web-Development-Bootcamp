b0f8a72a53c0fa2c903fcad2f542cdc8f7631098
M_SHAFEEQ_M <m_shafeeq_m@localhost.localdomain>
2024-03-04 17:23:33 -0100

    Blog API Completed

--- Back End/6.1 Blog API Project/index.js
+++ Back End/6.1 Blog API Project/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 4000;
+const port = 5000;
 
 // In-memory data store
 let posts = [
@@ -41,14 +41,57 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get("/posts",(req,res)=>{
+  res.json(posts)
+})
 
 //CHALLENGE 2: GET a specific post by id
 
+app.get("/posts/:id",(req,res)=>{
+  const post = posts.find((item)=> item.id === parseInt(req.params.id))
+  if(post) return res.json(post).status(200);
+  res.status(404).send("Message Not Found")
+})
+
 //CHALLENGE 3: POST a new post
+
+app.post("/posts",(req,res)=>{
+  const newId = lastId+1;
+   const newPost = {
+    id : newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date()
+  }
+  posts.push(newPost);
+  lastId=newId;
+  res.status(201).send("New Blog Created")
+
+})
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 
+app.patch("/posts/:id",(req,res)=>{
+  const post = posts.find((item)=> item.id === parseInt(req.params.id))
+  if (!post) return res.status(404).json({ message: "Post not found" });
+
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
+
+  res.status(200).json("Blog Updated")
+})
+
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+
+app.delete("/posts/:id", (req,res)=>{
+  const index = posts.findIndex((item)=> item.id === parseInt(req.params.id))
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+  posts.splice(index,1)
+  res.json({ message: "Post deleted" });
+
+})
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
--- Back End/6.1 Blog API Project/server.js
+++ Back End/6.1 Blog API Project/server.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "http://localhost:4000";
+const API_URL = "http://localhost:5000";
 
 app.use(express.static("public"));
 
@@ -40,7 +40,7 @@ app.get("/edit/:id", async (req, res) => {
     res.status(500).json({ message: "Error fetching post" });
   }
 });
-
+  
 // Create a new post
 app.post("/api/posts", async (req, res) => {
   try {
@@ -78,5 +78,5 @@ app.get("/api/posts/delete/:id", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Backend server is running on http://localhost:${port}`);
+  console.log(`Backend server is running on http://localhost:${port}`); 
 });
